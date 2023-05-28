2e61a91b2ad7683943aaeba563a54b6ee7ce4f34
Carlos <carlos@MacBook-Air-de-Carlos.local>
2023-05-28 13:44:34 +0200

    subiendo

--- proyecto-angular/src/app/component/form-login/form-login.component.ts
+++ proyecto-angular/src/app/component/form-login/form-login.component.ts
@@ -18,7 +18,15 @@ export class FormLoginComponent implements OnInit {
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6), this.validatePassword]]
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(12),
+          this.validatePassword
+        ]
+      ]
     });
   }
 
@@ -36,20 +44,27 @@ export class FormLoginComponent implements OnInit {
     this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
   }
 
+  onPasswordInput(event: any) {
+    const value = event.target.value;
+    if (value.length > 12) {
+      event.target.value = value.slice(0, 12); // Recortar el valor a 12 caracteres
+      this.loginForm.get('password').setValue(value.slice(0, 12)); // Actualizar el valor en el formulario
+    }
+  }
+
   onSubmit() {
     this.submitted = true;
-  
+
     if (this.loginForm.invalid) {
       this.loginForm.markAllAsTouched(); // Marcar todos los campos como "touched" para mostrar los mensajes de error
       return;
     }
-  
+
     // Lógica adicional para enviar el formulario o realizar otras acciones
     // ...
-  
-    // Reiniciar los campos y el formulario después de enviarlo 
+
+    // Reiniciar los campos y el formulario después de enviarlo
     this.loginForm.reset();
     this.submitted = false;
   }
-
 }
